1a4575c1dbc50dfc86c73c350b7eee442fb46ea1
Rama Q <qamra.j.khan@googlemail.comk>
2015-08-27 21:04:59 +0800

    first commit

---
+++ app/src/main/java/com/example/android/mydd2015/HomePageActivity.java
@@ -0,0 +1,169 @@
+package com.example.android.mydd2015;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class HomePageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+       // updateM2XURL();
+        M2XURLClass m2XURLClass=new M2XURLClass();
+        m2XURLClass.execute();
+       TextView textviewUrlPathTemp=(TextView) findViewById(R.id.m2xUrlPathTextView);
+        String  urlPathTemp="https://api-m2x.att.com/v2/devices/02h280h4j05536hh19k5il21i085997i/streams/temperature/values?start=2014-10-01T12:00:00Z&end=2014-10-03T12:00:00Z&limit=3&pretty%22%20-H%20%22X-M2X-KEY:%207611hg8391k834829gkff640j8j990i2%22";
+        textviewUrlPathTemp.setText(urlPathTemp);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void updateM2XURL() {
+        //M2XURLClass m2XURLClass=new M2XURLClass(this,"123");
+        //String test="beep beep";
+
+
+    }
+
+
+    public class M2XURLClass extends AsyncTask<Void, Void, String>{
+
+        public final String LOG_TAG = M2XURLClass.class.getSimpleName();
+
+
+
+        @Override
+        protected  String doInBackground(Void...params){
+
+            // If there's no zip code, there's nothing to look up.  Verify size of params.
+          //  if (params.length == 0) {
+           //     return null;
+           // }
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String FORECAST_BASE_URL ="https://api-m2x.att.com/v2/devices/02h280h4j05536hh19k5il21i085997i/streams/temperature/values?start=2014-10-01T12:00:00Z&end=2014-10-03T12:00:00Z&limit=3&pretty%22%20-H%20%22X-M2X-KEY:%207611hg8391k834829gkff640j8j990i2%22";
+                       // "http://api.openweathermap.org/data/2.5/forecast/daily?";
+               // final String QUERY_PARAM = "q";
+               // final String FORMAT_PARAM = "mode";
+               // final String UNITS_PARAM = "units";
+               // final String DAYS_PARAM = "cnt";
+
+                Uri builtUri =Uri.parse(FORECAST_BASE_URL);//.build(); //Uri.parse(FORECAST_BASE_URL).buildUpon()
+                       // .appendQueryParameter(QUERY_PARAM, params[0])
+                        //.appendQueryParameter(FORMAT_PARAM, format)
+                       // .appendQueryParameter(UNITS_PARAM, units)
+                       // .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        //.build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+
+                Log.v(LOG_TAG, "Forecast string: " + forecastJsonStr);
+            } catch (IOException e) {
+               Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+
+          return forecastJsonStr;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            TextView textview=(TextView) findViewById(R.id.m2xUrlTextView);
+            textview.setText(result);
+        }
+
+            }
+    }
+
---
+++ app/src/main/java/com/example/android/mydd2015/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.android.mydd2015;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void login(View view){
+        EditText username = (EditText)findViewById(R.id.loginUsername);
+        EditText password = (EditText)findViewById(R.id.loginPassword);
+        String usernameString=username.getText().toString();
+        String userPasswordString= password .getText().toString();
+
+        if(usernameString.equalsIgnoreCase("admin") && userPasswordString.equalsIgnoreCase("admin")){
+//go to another page
+            //correcct password
+            Intent launchDetailActivity=new Intent(this, HomePageActivity.class);///Intent(getActivity(), HomePageActivity.class);//.putExtra(Intent.EXTRA_TEXT,forecast);
+            startActivity(launchDetailActivity);
+
+        }else{
+            //wrong password--toast
+            Context context = getApplicationContext();
+            CharSequence text = "Wrong password, try again DICKHEAD!!!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+}
